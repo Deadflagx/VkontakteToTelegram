b17debfd243ffc2ddcb13db71ec0e2db1ff929fd
Deadflagx <68439718+Deadflagx@users.noreply.github.com>
2021-03-05 15:24:33 +0300

    Update settings.py

--- VkToTelegram/settings.py
+++ VkToTelegram/settings.py
@@ -1,5 +1,5 @@
-TELETOKEN = #Telegram Bot Token
-VKTOKEN = #VK token from group with enabled LongPoll API 
-	  #Scope: Wall_Post_New
-CHAT_ID = #Type str Telegram Chat ID 
-GROUP_ID = #Type int Group ID
+TELETOKEN = 	#Telegram Bot Token
+VKTOKEN = 	#VK token from group with enabled LongPoll API 
+	  	#Scope: Wall_Post_New
+CHAT_ID = 	#Type str Telegram Chat ID 
+GROUP_ID = 	#Type int Group ID
